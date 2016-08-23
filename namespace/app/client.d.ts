e9407b6e0225d6c703ca05c9ffca20d56e1346ef
Munar, Serkan <serkan.munar@hexagongeospatial.com>
2016-08-23 13:38:58 +0200

    Added solution

---
+++ namespace/app/client.d.ts
@@ -0,0 +1,19 @@
+﻿declare module namespace {
+    class Client {
+        static getData(cb: (data: IData) => void);
+    }
+}
+
+declare module namespace {
+    interface IData {
+        id: string;
+        type: DataType;
+    }
+}
+
+declare module namespace {
+    enum DataType {
+        TypeA = 0,
+        TypeB = 1
+    }
+}
---
+++ namespace/app/model.ts
@@ -0,0 +1,23 @@
+﻿module namespace.sub {
+    export interface IData {
+        id: string;
+        type: DataType;
+    }
+
+    export enum DataType {
+        TypeA,
+        TypeB,
+        //namespace.models.DataType != namespace.DataType, TypeC is missing in namespace.DataType
+        TypeC
+    }
+
+    export class Model {
+        data: IData;
+
+        constructor() {
+            Client.getData((data: IData) => {
+                this.data = data;
+            });
+        }
+    }
+}
